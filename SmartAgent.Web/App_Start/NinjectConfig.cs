d13c906d6becb2987ad1f8eab0a18e68cd89d0fa
kris1226 <kris1226@gmail.com>
2015-11-11 16:01:57 -0500

    Cleaner webapi project

---
+++ SmartAgent.Web/App_Start/NinjectConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using iAgentDataTool.Helpers.Interfaces;
+using iAgentDataTool.Models.Common;
+using iAgentDataTool.Repositories.Common;
+using iAgentDataTool.Models.SmartAgentModels;
+using iAgentDataTool.Repositories.AsyncRepositoires.SmartAgent;
+using iAgentDataTool.Repositories;
+using iAgentDataTool.AsyncRepositories.Common;
+
+namespace SmartAgent.Web.App_Start
+{
+    public class NinjectConfig
+    {
+        public static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                IDbConnection smartAgentDb = new SqlConnection(ConfigurationManager.ConnectionStrings["SmartAgentProd"].ConnectionString);
+
+                kernel.Bind<IAsyncRepository<ClientMaster>>().To<ClientMasterRepositoryAsync>().WithConstructorArgument("db", smartAgentDb);
+                kernel.Bind<IAsyncRepository<ClientLocations>>().To<ClientLocationsAsyncRepository>().WithConstructorArgument("db", smartAgentDb);
+                kernel.Bind<IAsyncRepository<WebsiteMaster>>().To<WebsiteMasterAsyncRepository>().WithConstructorArgument("db", smartAgentDb);
+                kernel.Bind<IAsyncRepository<FacilityMaster>>().To<FacilityMasterAsyncRepository>().WithConstructorArgument("db", smartAgentDb);
+                kernel.Bind<IAsyncRepository<FacilityDetail>>().To<FacilityDetialsAsyncRepository>().WithConstructorArgument("db", smartAgentDb);
+
+                kernel.Bind<IAsyncRepository<CriteriaSets>>().To<CriteriaSetsRepository>().WithConstructorArgument("db", smartAgentDb);
+                kernel.Bind<IAsyncRepository<CriteriaDetails>>().To<CriteriaDetialsRepository>().WithConstructorArgument("db", smartAgentDb);
+                kernel.Bind<IAsyncRepository<ScriptMaster>>().To<ScriptMasterRepository>().WithConstructorArgument("db", smartAgentDb);
+
+
+                return kernel;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ SmartAgent.Web/Controllers/WebsitesController.cs
@@ -0,0 +1,76 @@
+﻿using iAgentDataTool.Helpers.Interfaces;
+using iAgentDataTool.Models.Common;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace SmartAgent.Web.Controllers
+{
+    public class WebsitesController : ApiController
+    {
+        [Inject]
+        IAsyncRepository<WebsiteMaster> _websiteRepo;
+
+        [Inject]
+        public WebsitesController(IAsyncRepository<WebsiteMaster> websiteRepo)
+        {
+            this._websiteRepo = websiteRepo;
+        }
+
+        [HttpGet]
+        [Route("api/websites")]
+        [ResponseType(typeof(WebsiteMaster))]
+        public async Task<IHttpActionResult> GetAllWebsites()
+        {
+            var websites = await _websiteRepo.GetAllAsync();
+            if (websites.Any())
+            {
+                return this.Ok(websites);
+            }
+            else
+            {
+                return this.NotFound();
+            }
+        }
+        [HttpGet]
+        [Route("api/websites/{websiteDescription}")]
+        [ResponseType(typeof(WebsiteMaster))]
+        public async Task<IHttpActionResult> GetWebsite(string wildCard)
+        {
+            var websites = await _websiteRepo.FindByName(wildCard);
+            if (websites.Any())
+            {
+                return Ok(websites);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [HttpPost]
+        [Route("api/websites")]
+        [ResponseType(typeof(WebsiteMaster))]
+        public async Task<IHttpActionResult> PostWebsite(IEnumerable<WebsiteMaster> newWebsite)
+        {
+            foreach (var item in newWebsite)
+            {
+                var response = Request.CreateResponse<WebsiteMaster>(HttpStatusCode.Created, item);
+                if (item.WebsiteDescription != null)
+                {
+                    var key = await _websiteRepo.AddAsync(newWebsite);
+                    string uri = Url.Link("DefaultApi", new { key });
+                    response.Headers.Location = new Uri(uri);
+                    return this.Ok();
+                }
+                return this.BadRequest();
+            }
+            return this.BadRequest();
+        }
+    }
+}
